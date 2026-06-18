cbd08b03480e121b2a1c463e5ad08f635f224347
vikash sharma <vikashtiwari9893@gmail.com>
2026-06-18 15:55:04 +0530

    server live api or render added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,3 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-
-// // export default defineConfig({
-// //   plugins: [react()],
-// //   server: {
-// //     port: 5173,
-// //   },
-// // })
-
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
@@ -16,14 +5,7 @@
 //   plugins: [react()],
 //   server: {
 //     port: 5173,
-//     proxy: {
-//       '/api': {
-//         target: 'https://mern-linkedin-post-generator-multiagents.onrender.com',
-//         changeOrigin: true,
-//         secure: true,
-//       }
-//     }
-//   }
+//   },
 // })
 
 
