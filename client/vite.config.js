1b910171ab2e4c5fdfaf5972fcfd4e13b3379a26
vikash sharma <vikashtiwari9893@gmail.com>
2026-06-18 19:40:37 +0530

    code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,28 @@
+// // // // import { defineConfig } from 'vite'
+// // // // import react from '@vitejs/plugin-react'
+
+// // // // export default defineConfig({
+// // // //   plugins: [react()],
+// // // //   server: {
+// // // //     port: 5173,
+// // // //   },
+// // // // })
+
+
 // // // import { defineConfig } from 'vite'
 // // // import react from '@vitejs/plugin-react'
 
 // // // export default defineConfig({
 // // //   plugins: [react()],
 // // //   server: {
-// // //     port: 5173,
-// // //   },
+// // //     proxy: {
+// // //       '/api': {
+// // //         target: 'https://mern-linkedin-post-generator-multiagents.onrender.com',
+// // //         changeOrigin: true,
+// // //       }
+// // //     }
+// // //   }
 // // // })
-
-
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-
-// // export default defineConfig({
-// //   plugins: [react()],
-// //   server: {
-// //     proxy: {
-// //       '/api': {
-// //         target: 'https://mern-linkedin-post-generator-multiagents.onrender.com',
-// //         changeOrigin: true,
-// //       }
-// //     }
-// //   }
-// // })
 
 
 
@@ -52,9 +52,23 @@
 
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://mern-linkedin-post-generator-multiagents.onrender.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
+})
